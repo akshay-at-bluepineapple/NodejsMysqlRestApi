9d7492e91ccacaff02933728908763bd2944ba0d
akshay-at-bluepineapple <akshay.vishwakarma@bluepineapple.com>
2022-12-13 12:31:04 +0530

    "minor changes"

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const userRouter = require("./api/users/userRouter");
 app.use(express.json()); //as user is passing the json object hence need to convert it to json explicitly
 //user router
 app.use("/api/users",userRouter);
+app.use("/",(req,res)=>{
+  res.json({
+    message:"mewelcome to NodeJs-Mysql RestApi",
+    routes:"/api/users"
+  });
+})
 
 app.listen(process.env.APP_PORT, () => {
   console.log("[-] server running on 3000");
