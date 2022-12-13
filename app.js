99554641be6bf7e3a771f28a6e34b7ce99529b53
akshay-at-bluepineapple <akshay.vishwakarma@bluepineapple.com>
2022-12-13 20:33:23 +0530

    "solving cors error"

--- app.js
+++ app.js
@@ -2,26 +2,37 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
 const userRouter = require("./api/users/userRouter");
+
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 
 app.use(express.json()); //as user is passing the json object hence need to convert it to json explicitly
 //user router
-app.use(cors({
-  origin: '*'
-}));
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
 
 
-app.use("/api/users",userRouter);
 
+app.use("/api/users", userRouter);
 
-app.use("/",(req,res)=>{
+app.use("/", (req, res) => {
   res.json({
-    message:"mewelcome to NodeJs-Mysql RestApi",
-    routes:"/api/users"
+    message: "mewelcome to NodeJs-Mysql RestApi",
+    routes: "/api/users",
   });
-})
+});
 
 app.listen(process.env.APP_PORT, () => {
   console.log("[-] server running on 3000");
