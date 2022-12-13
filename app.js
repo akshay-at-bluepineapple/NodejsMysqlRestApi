17e0cf9fc188a7b96a164ad96156d13cbf4af3bf
akshay-at-bluepineapple <akshay.vishwakarma@bluepineapple.com>
2022-12-13 20:55:48 +0530

    "solving cors -1"

--- app.js
+++ app.js
@@ -5,25 +5,14 @@ const app = express();
 const cors = require("cors");
 const userRouter = require("./api/users/userRouter");
 
-
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
-
+app.use(
+  cors({
+    origin: "https://nodejs-mysql-rest-api.vercel.app/",
+  })
+);
 
 app.use(express.json()); //as user is passing the json object hence need to convert it to json explicitly
 //user router
-// app.use(
-//   cors({
-//     origin: "*",
-//   })
-// );
-
-
 
 app.use("/api/users", userRouter);
 
